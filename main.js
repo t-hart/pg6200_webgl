e33d30bdb0b7d5e69e3119cd535450dc2bfe0ea8
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-08-23 19:00:40 +0200

    finishes applying colors

---
+++ main.js
@@ -0,0 +1,166 @@
+/* global alert */
+import { mat4 } from './gl-matrix/lib/gl-matrix.js'
+
+const vsSource = `
+  attribute vec4 aVertexPosition;
+  attribute vec4 aVertexColor;
+
+  uniform mat4 uModelViewMatrix;
+  uniform mat4 uProjectionMatrix;
+
+  varying highp vec4 vColor;
+
+  void main() {
+    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    vColor = aVertexColor;
+  }
+`
+
+const fsSource = `
+  varying highp vec4 vColor;
+
+  void main() {
+    gl_FragColor = vColor;
+  }
+`
+
+const initShaderProgram = (gl, vsSource, fsSource) => {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource)
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource)
+
+  const shaderProgram = gl.createProgram()
+  gl.attachShader(shaderProgram, vertexShader)
+  gl.attachShader(shaderProgram, fragmentShader)
+  gl.linkProgram(shaderProgram)
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert('Unable to init shader program:', gl.getProgramInfoLog(shaderProgram))
+    return null
+  }
+
+  return shaderProgram
+}
+
+const loadShader = (gl, type, source) => {
+  const shader = gl.createShader(type)
+  gl.shaderSource(shader, source)
+  gl.compileShader(shader)
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert('An error occurred while compiling the shaders: ', gl.getShaderInfoLog(shader))
+    gl.deleteShader(shader)
+    return null
+  }
+  return shader
+}
+
+const initBuffers = (gl) => {
+  const createBuffer = (arr) => {
+    const buffer = gl.createBuffer()
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arr), gl.STATIC_DRAW)
+    return buffer
+  }
+
+  const positions = [
+    -1.0, 1.0,
+    1.0, 1.0,
+    -1.0, -1.0,
+    1.0, -1.0
+  ]
+
+  const colors = [
+    1.0, 1.0, 1.0, 1.0, // white
+    1.0, 0.0, 0.0, 1.0, // red
+    0.0, 1.0, 0.0, 1.0, // green
+    0.0, 0.0, 1.0, 1.0 // blue
+  ]
+
+  return {
+    position: createBuffer(positions),
+    color: createBuffer(colors)
+  }
+}
+
+const drawScene = (gl, programInfo, buffers) => {
+  gl.clearColor(0, 0, 0, 1)
+  gl.clearDepth(1)
+  gl.enable(gl.DEPTH_TEST)
+  gl.depthFunc(gl.LEQUAL)
+
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+  const fieldOfView = 45 * Math.PI / 180
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight
+  const zNear = 0.1
+  const zFar = 100
+  const projectionMatrix = mat4.create()
+  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar)
+
+  const modelViewMatrix = mat4.create()
+
+  mat4.translate(modelViewMatrix, modelViewMatrix, [0, 0, -6])
+
+  const bindBuffer = (numComponents, buffer, attribLocs) => {
+    const type = gl.FLOAT
+    const normalize = false
+    const stride = 0
+    const offset = 0
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+    gl.vertexAttribPointer(
+      attribLocs,
+      numComponents,
+      type,
+      normalize,
+      stride,
+      offset
+    )
+    gl.enableVertexAttribArray(attribLocs)
+  }
+
+  bindBuffer(4, buffers.color, programInfo.attribLocations.vertexColor)
+  bindBuffer(2, buffers.position, programInfo.attribLocations.vertexPosition)
+
+  gl.useProgram(programInfo.program)
+
+  gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix)
+
+  gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix)
+
+  {
+    const offset = 0
+    const vertexCount = 4
+    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount)
+  }
+}
+
+const main = () => {
+  const canvas = document.querySelector('#glCanvas')
+  const gl = canvas.getContext('webgl')
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource)
+
+  const programInfo = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      vertexColor: gl.getAttribLocation(shaderProgram, 'aVertexColor')
+    },
+    uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix')
+    }
+  }
+
+  if (!gl) {
+    alert('Unable to init webgl')
+    return
+  }
+
+  gl.clearColor(0, 0, 0, 1)
+  gl.clear(gl.COLOR_BUFFER_BIT)
+
+  const buffers = initBuffers(gl)
+
+  drawScene(gl, programInfo, buffers)
+}
+
+main()
