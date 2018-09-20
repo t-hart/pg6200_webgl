6e58b92a01ca5e34e9b7066daa11c838b5cb464b
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-09-20 16:56:29 +0200

    Fixes texture issue

--- src/models.ts
+++ src/models.ts
@@ -2,6 +2,7 @@ import objs, { ObjData, ObjTexture } from './objs'
 import { initTexture2D } from './textureUtils'
 import { initShaderProgram } from './shaderUtils'
 import { availableShaders, Programs } from './shaders'
+import { log } from './index'
 
 export type ModelData = {
   objData: ObjData,
@@ -16,7 +17,7 @@ export const defaultProgram = "color"
 const models = (gl: WebGLRenderingContext) => mapObject(objs, (x: ObjTexture) => ({
   objData: x.model,
   programs: mapObject(availableShaders(x.model), initShaderProgram(gl)),
-  texture: x.texturePath ? initTexture2D(gl)(x.texturePath) : null
+  texture: x.texturePath ? log(initTexture2D(gl)(x.texturePath)) : null
 }))
 
 export default models
