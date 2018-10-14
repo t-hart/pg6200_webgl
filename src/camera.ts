b50fe2ed14cec5779415b266b75dec49632b89ab
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-10-14 07:15:09 +0200

    Implements basic movement and rotation with quats

--- src/camera.ts
+++ src/camera.ts
@@ -7,11 +7,6 @@ type Quaternion = number[] | Float32Array
 const VELOCITY = .5
 const ROTATION = VELOCITY * .25
 
-const axisFromMatrix = (x: number, y: number, z: number) => (m: Matrix) => ([m[x], m[y], m[z]])
-const XM = axisFromMatrix(0, 4, 8)
-const YM = axisFromMatrix(1, 5, 9)
-const ZM = axisFromMatrix(2, 6, 10)
-
 const rotateVec = (v: Vector) => (q: Quaternion) => vec3.transformQuat(vec3.create(), v, q)
 const X = rotateVec([-1, 0, 0])
 const Y = rotateVec([0, -1, 0])
@@ -19,47 +14,41 @@ const Z = rotateVec([0, 0, -1])
 
 type Camera = {
   translation: Vector,
-  translationM: Vector,
   rotation: Quaternion,
   viewMatrix: Matrix
 }
 
 export const create = (pos: Vector = [0, 0, -6], rotation: Quaternion = [0, 0, 0, 1]) => ({
   translation: pos,
-  translationM: pos,
   rotation: rotation,
-  viewMatrix: mat4.fromTranslation(mat4.create(), pos)
 })
 
-const translate = (camera: Camera, axis: Function, axisM: Function) => ({
+const translate = (camera: Camera, axis: Function) => ({
   ...camera,
   translation: add(camera.translation)(scale(VELOCITY)(axis(camera.rotation))),
-  translationM: add(camera.translationM)(scale(VELOCITY)(axisM(camera.viewMatrix))),
-  viewMatrix: mat4.translate(mat4.create(), camera.viewMatrix, scale(VELOCITY)(axisM(camera.viewMatrix)))
 })
 
-const rotate = (axis: Function, axisM: Function) => (camera: Camera, rad: number) => ({
+const rotate = (axis: Function) => (camera: Camera, rad: number) => ({
   ...camera,
   rotation: quat.multiply(quat.create(), quat.setAxisAngle(quat.create(), axis(camera.rotation), rad), camera.rotation),
-  viewMatrix: mat4.rotate(mat4.create(), camera.viewMatrix, rad, axisM(camera.viewMatrix))
 })
 
 
 // translation
 const neg = (f: Function) => (v: Vector) => negate(f(v))
-export const moveForward = (camera: Camera) => translate(camera, neg(Z), ZM)
-export const moveBackward = (camera: Camera) => translate(camera, Z, neg(ZM))
+export const moveForward = (camera: Camera) => translate(camera, neg(Z))
+export const moveBackward = (camera: Camera) => translate(camera, Z)
 
-export const moveRight = (camera: Camera) => translate(camera, X, neg(XM))
-export const moveLeft = (camera: Camera) => translate(camera, neg(X), XM)
+export const moveRight = (camera: Camera) => translate(camera, X)
+export const moveLeft = (camera: Camera) => translate(camera, neg(X))
 
-export const moveUp = (camera: Camera) => translate(camera, Y, neg(YM))
-export const moveDown = (camera: Camera) => translate(camera, neg(Y), YM)
+export const moveUp = (camera: Camera) => translate(camera, Y)
+export const moveDown = (camera: Camera) => translate(camera, neg(Y))
 
 // rotation
-const roll = rotate(Z, ZM)
-const pitch = rotate(X, XM)
-const yaw = rotate(Y, YM)
+const roll = rotate(Z)
+const pitch = rotate(X)
+const yaw = rotate(Y)
 
 export const rollLeft = (camera: Camera) => roll(camera, -ROTATION)
 export const rollRight = (camera: Camera) => roll(camera, ROTATION)
@@ -71,8 +60,13 @@ export const tiltUp = (camera: Camera) => pitch(camera, -ROTATION)
 export const tiltDown = (camera: Camera) => pitch(camera, ROTATION)
 
 // export const rotation = (camera: Camera) => camera.rotation
-// export const translationM = (camera: Camera) => camera.translationM
-export const rotation = (camera: Camera) => mat4.getRotation(quat.create(), camera.viewMatrix)
+// export const rotation = (camera: Camera) => mat4.getRotation(quat.create(), camera.viewMatrix)
+export const rotation = (camera: Camera) => {
+  // const matRotation = mat4.getRotation(quat.create(), camera.viewMatrix)
+  const con = quat.conjugate(quat.create(), camera.rotation)
+  // console.assert(quat.equals(con, matRotation), 'Rotations do not match', con, matRotation)
+  return con
+}
 export const translation = (camera: Camera) => camera.translation
 
 export default Camera;
