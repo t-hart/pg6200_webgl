43a40603a3c90b384d8fa705c0308321a55cee56
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-10-14 07:38:05 +0200

    Cleans up camera controls

--- src/camera.ts
+++ src/camera.ts
@@ -1,21 +1,19 @@
-import { mat4, vec3, quat } from 'gl-matrix-ts'
+import { vec3, quat } from 'gl-matrix-ts'
 import Vector, { negate, scale, add } from './vector'
 
-type Matrix = number[] | Float32Array
 type Quaternion = number[] | Float32Array
 
 const VELOCITY = .5
 const ROTATION = VELOCITY * .25
 
 const rotateVec = (v: Vector) => (q: Quaternion) => vec3.transformQuat(vec3.create(), v, q)
-const X = rotateVec([-1, 0, 0])
-const Y = rotateVec([0, -1, 0])
-const Z = rotateVec([0, 0, -1])
+const X = rotateVec([1, 0, 0])
+const Y = rotateVec([0, 1, 0])
+const Z = rotateVec([0, 0, 1])
 
 type Camera = {
   translation: Vector,
   rotation: Quaternion,
-  viewMatrix: Matrix
 }
 
 export const create = (pos: Vector = [0, 0, -6], rotation: Quaternion = [0, 0, 0, 1]) => ({
@@ -36,37 +34,30 @@ const rotate = (axis: Function) => (camera: Camera, rad: number) => ({
 
 // translation
 const neg = (f: Function) => (v: Vector) => negate(f(v))
-export const moveForward = (camera: Camera) => translate(camera, neg(Z))
-export const moveBackward = (camera: Camera) => translate(camera, Z)
+export const moveForward = (camera: Camera) => translate(camera, Z)
+export const moveBackward = (camera: Camera) => translate(camera, neg(Z))
 
-export const moveRight = (camera: Camera) => translate(camera, X)
-export const moveLeft = (camera: Camera) => translate(camera, neg(X))
+export const moveRight = (camera: Camera) => translate(camera, neg(X))
+export const moveLeft = (camera: Camera) => translate(camera, X)
 
-export const moveUp = (camera: Camera) => translate(camera, Y)
-export const moveDown = (camera: Camera) => translate(camera, neg(Y))
+export const moveUp = (camera: Camera) => translate(camera, neg(Y))
+export const moveDown = (camera: Camera) => translate(camera, Y)
 
 // rotation
 const roll = rotate(Z)
 const pitch = rotate(X)
 const yaw = rotate(Y)
 
-export const rollLeft = (camera: Camera) => roll(camera, -ROTATION)
-export const rollRight = (camera: Camera) => roll(camera, ROTATION)
+export const rollLeft = (camera: Camera) => roll(camera, ROTATION)
+export const rollRight = (camera: Camera) => roll(camera, -ROTATION)
 
-export const turnRight = (camera: Camera) => yaw(camera, ROTATION)
-export const turnLeft = (camera: Camera) => yaw(camera, -ROTATION)
+export const turnRight = (camera: Camera) => yaw(camera, -ROTATION)
+export const turnLeft = (camera: Camera) => yaw(camera, ROTATION)
 
-export const tiltUp = (camera: Camera) => pitch(camera, -ROTATION)
-export const tiltDown = (camera: Camera) => pitch(camera, ROTATION)
+export const tiltUp = (camera: Camera) => pitch(camera, ROTATION)
+export const tiltDown = (camera: Camera) => pitch(camera, -ROTATION)
 
-// export const rotation = (camera: Camera) => camera.rotation
-// export const rotation = (camera: Camera) => mat4.getRotation(quat.create(), camera.viewMatrix)
-export const rotation = (camera: Camera) => {
-  // const matRotation = mat4.getRotation(quat.create(), camera.viewMatrix)
-  const con = quat.conjugate(quat.create(), camera.rotation)
-  // console.assert(quat.equals(con, matRotation), 'Rotations do not match', con, matRotation)
-  return con
-}
+export const rotation = (camera: Camera) => quat.conjugate(quat.create(), camera.rotation)
 export const translation = (camera: Camera) => camera.translation
 
 export default Camera;
