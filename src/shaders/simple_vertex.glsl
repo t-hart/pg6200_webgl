d6e41706da99f6a08e5ff9b0f6c49a730925f6e0
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2018-10-21 09:44:11 +0200

    Minor shader tweaks

--- src/shaders/simple_vertex.glsl
+++ src/shaders/simple_vertex.glsl
@@ -16,12 +16,13 @@ void main(void) {
     gl_Position = uProjectionMatrix * uViewMatrix * uModelMatrix * aVertexPosition;
     vColor = aVertexColor * uColorMult;
 
-    highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
+    highp vec3 ambientLight = vec3(0.5, 0.5, 0.5);
     highp vec3 directionalLightColor = vec3(1, 1, 1);
     highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
 
     highp vec4 transformedNormal = normalize(uNormalMatrix * vec4(aVertexNormal, 1.0));
 
-    highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+    // highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+    highp float directional = dot(transformedNormal.xyz, directionalVector);
     vLighting = ambientLight + (directionalLightColor * directional);
 }
